dfa3d69e020c354548109caee5d69a25d4d47f3a
WildTooth <champenv@outlook.dk>
2024-02-20 21:39:49 +0100

    Tilføjet DiscordSubConfiguration.java

---
+++ core/src/main/java/org/champenslabyaddons/fvp/configuration/DiscordSubConfiguration.java
@@ -0,0 +1,16 @@
+package org.champenslabyaddons.fvp.configuration;
+
+import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
+import net.labymod.api.configuration.loader.Config;
+import net.labymod.api.configuration.loader.annotation.ParentSwitch;
+import net.labymod.api.configuration.loader.property.ConfigProperty;
+
+public class DiscordSubConfiguration extends Config {
+  @ParentSwitch
+  @SwitchSetting
+  private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
+
+  public ConfigProperty<Boolean> enabled() {
+    return this.enabled;
+  }
+}
