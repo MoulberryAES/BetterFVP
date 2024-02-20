137b6c895e62879215e89d8687ea19650b60d627
WildTooth <champenv@outlook.dk>
2024-02-20 21:43:39 +0100

    Tilføjet DiscordSubConfiguration til FreakyVillePlusConfiguration.java

--- core/src/main/java/org/champenslabyaddons/fvp/FreakyVillePlusConfiguration.java
+++ core/src/main/java/org/champenslabyaddons/fvp/FreakyVillePlusConfiguration.java
@@ -4,6 +4,7 @@ import net.labymod.api.addon.AddonConfig;
 import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
 import net.labymod.api.configuration.loader.annotation.ConfigName;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
+import org.champenslabyaddons.fvp.configuration.DiscordSubConfiguration;
 
 @ConfigName("settings")
 public class FreakyVillePlusConfiguration extends AddonConfig {
@@ -11,8 +12,14 @@ public class FreakyVillePlusConfiguration extends AddonConfig {
   @SwitchSetting
   private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
 
+  private final DiscordSubConfiguration discordSubSettings = new DiscordSubConfiguration();
+
   @Override
   public ConfigProperty<Boolean> enabled() {
     return this.enabled;
+  }
+
+  public DiscordSubConfiguration getDiscordSubSettings() {
+    return this.discordSubSettings;
   }
 }
