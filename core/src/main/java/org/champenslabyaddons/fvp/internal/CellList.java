2fdaf8e67968593674a008cd7a6fd0ea7b727c1e
WildTooth <champenV@outlook.dk>
2024-02-22 16:59:09 +0100

    CellList virker nu til formålet.

--- core/src/main/java/org/champenslabyaddons/fvp/internal/CellList.java
+++ core/src/main/java/org/champenslabyaddons/fvp/internal/CellList.java
@@ -1,12 +1,17 @@
 package org.champenslabyaddons.fvp.internal;
 
 import org.champenslabyaddons.fvp.objects.CellBlock;
+import org.champenslabyaddons.fvp.util.csv.Csv;
 import org.champenslabyaddons.fvp.util.http.HttpConnector;
+import org.champenslabyaddons.fvp.util.resource.Resources;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Denne klasse håndtere listen over forskellige Cellegange / Områder.
@@ -20,7 +25,32 @@ public class CellList implements Manager {
 
   @Override
   public void init() throws IOException {
-    HttpURLConnection connection = HttpConnector.getOpenHttpConnection(new URL(""));
+    HttpURLConnection connection = HttpConnector.getOpenHttpConnection(new URL(
+        Resources.CELL_LIST_URL));
+    Csv csv = new Csv(new BufferedReader(new InputStreamReader(connection.getInputStream())));
 
+    for (String[] strings : csv.readALl()) {
+      this.cellBlocks.add(new CellBlock(strings[0], Integer.parseInt(strings[1]), Integer.parseInt(strings[2]), strings[3]));
+    }
+
+    connection.disconnect();
+  }
+
+  public boolean isCellListed(String typeAndId) {
+    for (CellBlock cellBlock : this.cellBlocks) {
+      if (cellBlock.isCellPartOfCellBlock(typeAndId)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public Optional<CellBlock> getCorrespondingCellBlock(String typeAndId) {
+    for (CellBlock cellBlock : this.cellBlocks) {
+      if (cellBlock.isCellPartOfCellBlock(typeAndId)) {
+        return Optional.of(cellBlock);
+      }
+    }
+    return Optional.empty();
   }
 }
