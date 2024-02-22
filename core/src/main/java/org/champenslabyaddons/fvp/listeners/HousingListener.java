b85aac1fd51c164e654d8abd5033404ec089d142
WildTooth <champenV@outlook.dk>
2024-02-22 17:00:00 +0100

    Ændret strukturen i CellListener.java og tilføjet superklassen HousingListener.java

---
+++ core/src/main/java/org/champenslabyaddons/fvp/listeners/HousingListener.java
@@ -0,0 +1,14 @@
+package org.champenslabyaddons.fvp.listeners;
+
+import net.labymod.api.client.component.Component;
+import net.labymod.api.event.client.chat.ChatReceiveEvent;
+import net.labymod.api.util.Pair;
+
+public abstract class HousingListener {
+
+  public abstract void onChatReceived(ChatReceiveEvent event);
+
+  protected abstract Component objectLocation(String header);
+
+  protected abstract Pair<String, String> headerDecoration();
+}
--- core/src/main/java/org/champenslabyaddons/fvp/listeners/nprison/CellListener.java
+++ core/src/main/java/org/champenslabyaddons/fvp/listeners/nprison/CellListener.java
@@ -1,43 +1,62 @@
 package org.champenslabyaddons.fvp.listeners.nprison;
 
 import net.labymod.api.client.component.Component;
+import net.labymod.api.client.component.TextComponent;
 import net.labymod.api.event.Subscribe;
 import net.labymod.api.event.client.chat.ChatReceiveEvent;
 import net.labymod.api.util.Pair;
 import org.champenslabyaddons.fvp.connection.ClientInfo;
+import org.champenslabyaddons.fvp.internal.CellList;
+import org.champenslabyaddons.fvp.listeners.HousingListener;
+import org.champenslabyaddons.fvp.objects.CellBlock;
 import org.champenslabyaddons.fvp.util.FreakyVilleServer;
 import org.champenslabyaddons.fvp.util.Messaging;
 
-public class CellListener {
+import java.util.Objects;
 
+public final class CellListener extends HousingListener {
   private final ClientInfo clientInfo;
+  private final CellList cellList;
 
-  public CellListener(ClientInfo clientInfo) {
+  public CellListener(ClientInfo clientInfo, CellList cellList) {
     this.clientInfo = clientInfo;
+    this.cellList = cellList;
   }
 
   @Subscribe
+  @Override
   public void onChatReceived(ChatReceiveEvent event) {
     if (!this.clientInfo.isOnFreakyVille()
         && this.clientInfo.getCurrentServer() == FreakyVilleServer.PRISON) {
       return;
     }
     String plainMessage = event.chatMessage().getPlainText().trim();
-    if (plainMessage.startsWith(headerDecoration().getFirst()) &&
-        plainMessage.endsWith(headerDecoration().getSecond())) {
-      Messaging.executor().displayClientMessage(cellLocation(plainMessage));
+    if (plainMessage.startsWith(Objects.requireNonNull(headerDecoration().getFirst())) &&
+        plainMessage.endsWith(Objects.requireNonNull(headerDecoration().getSecond()))) {
+      Messaging.executor().displayClientMessage(objectLocation(plainMessage));
     }
   }
 
-  private Component cellLocation(String header) {
+  @Override
+  protected Component objectLocation(String header) {
     String restProduct = header
-        .replace(headerDecoration().getFirst(), "")
-        .replace(headerDecoration().getSecond(), "")
+        .replace(Objects.requireNonNull(headerDecoration().getFirst()), "")
+        .replace(Objects.requireNonNull(headerDecoration().getSecond()), "")
         .trim();
-    return Component.empty(); // MIDLERTIDIGT
+    if (cellList.isCellListed(restProduct)) {
+      CellBlock cellBlock;
+      if (cellList.getCorrespondingCellBlock(restProduct).isPresent()) {
+        cellBlock = cellList.getCorrespondingCellBlock(restProduct).get();
+      } else {
+        return Component.empty();
+      }
+      TextComponent cellComponent = Component.text(cellBlock.getLocationDescription());
+      return Component.translatable("fvp.server.prison.cell.location", cellComponent);
+    }
+    return Component.empty();
   }
 
-  private Pair<String, String> headerDecoration() {
+  protected Pair<String, String> headerDecoration() {
     return Pair.of("------------={", "}=------------");
   }
 }
