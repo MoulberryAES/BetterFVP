ef015a6991d70b2e0039fddb5ca8809d15c1b01d
WildTooth <champenv@outlook.dk>
2024-02-23 01:15:20 +0100

    Tilføjet linket til celle-listen

--- core/src/main/java/org/champenslabyaddons/fvp/util/resource/Resources.java
+++ core/src/main/java/org/champenslabyaddons/fvp/util/resource/Resources.java
@@ -3,5 +3,5 @@ package org.champenslabyaddons.fvp.util.resource;
 public class Resources {
   private Resources() {}
 
-  public static final String CELL_LIST_URL = "";
+  public static final String CELL_LIST_URL = "https://raw.githubusercontent.com/WildTooth/FreakyVille-General-Data/main/entities/cellblock.csv";
 }
